ed2a91413947c1b780a512e8bf347957d6948566
Victor Nogueira <69355100+victorftrdba@users.noreply.github.com>
2022-01-04 00:13:33 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://localhost:dev102030@cluster0.mdupn.mongodb.net/test?retryWrites=true&w=majority'),
+  imports: [MongooseModule.forRoot('mongodb+srv://localhost:PASSWORD@cluster0.mdupn.mongodb.net/DB?retryWrites=true&w=majority'),
     UsersModule],
   controllers: [AppController],
   providers: [AppService],
